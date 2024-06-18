f22a335fbfda818b1d4e4d9df17c0ef1cd3f931f
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-18 16:51:45 +0530

    Add files via upload

---
+++ promt.js
@@ -0,0 +1,5 @@
+let firstname=prompt("plese enter First name")
+let lastname=prompt("plese enter last name")
+
+console.log("Welocome "+firstname+" "+lastname)
+alert("Welocome "+firstname+" "+lastname)
