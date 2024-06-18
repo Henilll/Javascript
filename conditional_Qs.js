ed677d5f1141db08a777f667acbc87b3fdefe67b
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-18 16:38:17 +0530

    Add files via upload

---
+++ conditional_Qs.js
@@ -0,0 +1,14 @@
+//make a programe working of traffic singal lights
+let color='GREEN';
+
+if(color=="RED"){
+    console.log('Stop')
+}
+else if(color=="YELLOW"){
+    console.log("Slow Down")
+}
+else if(color=='GREEN'){
+    console.log("Go")
+
+}
+
