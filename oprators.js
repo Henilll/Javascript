c5a60d89de0b1e4d88679e189a1deb38a71c7e71
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-18 16:22:43 +0530

    Add files via upload

---
+++ oprators.js
@@ -0,0 +1,25 @@
+let a=10;
+let b=5;
+console.log("Uninary Oprators")
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+
+
+console.log(a--)
+console.log(a++)
+
+console.log("Comparison Oprators")
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log("using unicodes")
+console.log('p'>'q')
+
+
+
---
+++ print.js
@@ -0,0 +1,2 @@
+console.log("hello henjfnfnnnnnnn")
+document.write("hell0")
