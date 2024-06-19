86d74454933e76701b0045368182262fa3dd93f6
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-19 12:23:44 +0530

    array_methods.js

--- array_methods.js
+++ array_methods.js
@@ -28,3 +28,34 @@ a.shift()
 a.unshift('june')
 a.unshift('july')
 console.log(a)
+
+
+let b=['yellow','red','black']
+
+console.log(b.indexOf('yellow'))
+console.log(b.indexOf('green'))
+console.log(b.includes('yellow'))
+console.log(b.includes('green'))
+
+c=cars.concat(b)
+console.log(c)
+
+console.log(c.reverse())
+
+console.log(c.slice(2))
+console.log(c.slice(2,5))
+
+console.log(c.slice(-2))
+console.log(c.splice(4))
+console.log(c.splice(0,1,'black','grey'))
+console.log(c)
+
+let num=[3,48,63,1,0,84]
+console.log(num)
+console.log(num.sort())
+console.log(c.sort())
+
+
+
+let tic=[['x','','0'],['','x',''],['0','','x']]
+console.log(tic)
