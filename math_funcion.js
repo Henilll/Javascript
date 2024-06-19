2c7cf4c92f2933ed4b3293701eeb164761cea8f3
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-19 18:48:10 +0530

    Add files via upload

---
+++ math_funcion.js
@@ -0,0 +1,10 @@
+document.write(Math.PI+"<br>")
+document.write(Math.E+"<br>")
+document.write(Math.min(10,20)+"<br>")
+document.write(Math.abs(-12)+"<br>")
+document.write(Math.pow(2,3)+"<br>")
+document.write(Math.floor(3.99)+"<br>")
+document.write(Math.ceil(3.19)+"<br>")
+document.write(Math.random(0,1)+"<br>")
+
+
