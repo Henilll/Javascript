d344a59d352853b8ef6372aa5af32ca632abf0ca
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-06-19 11:49:05 +0530

    Add files via upload

---
+++ string_methods.js
@@ -0,0 +1,13 @@
+let msg='   hEllo    '
+console.log(msg.length)
+console.log(msg.trim())
+console.log(msg.toLowerCase())
+console.log(msg.toUpperCase())
+console.log(msg.indexOf('l'))
+console.log(msg.toLowerCase().trim())
+
+console.log(msg.slice(4))
+console.log(msg.slice(4,6))
+console.log(msg.slice(-7))
+
+console.log(msg.replace('h','How Are youu'))
