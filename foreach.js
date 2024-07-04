fd902f3cfdc98f340c32462ef20c4dd9e37ac2af
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-07-04 15:51:25 +0530

    Add files via upload

---
+++ foreach.js
@@ -0,0 +1,14 @@
+let arr = [{
+    name: "HEnil",
+    marks: 89,
+}, {
+    name: "HEnil",
+    marks: 89,
+}, {
+    name: "HEnil",
+    marks: 89,
+}]
+
+arr.forEach(element => {
+    document.write(element)
+});
