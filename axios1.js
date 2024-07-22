5a83e22cf7f2a340b4ecdbe851d268f9a7f4348e
Bhavsar Henil A <136498576+Henilll@users.noreply.github.com>
2024-07-22 18:45:14 +0530

    Add files via upload

---
+++ axios1.js
@@ -0,0 +1,16 @@
+const url="https://icanhazdadjoke.com/";
+
+async function getJokes(){
+    try{
+        const config={headers: { Accept :"application/json"}}
+        let res=await axios.get(url,config)
+        console.log(res.data);
+
+    }
+    catch(e){
+        console.log(e)
+
+    }
+}
+
+getJokes()
